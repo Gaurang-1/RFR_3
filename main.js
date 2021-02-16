be3dd025d8209a3d1262f10a7dcdef99d1ad2171
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-02-16 20:50:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ function setup() {
     canvas.center();
     video=createCapture(VIDEO);
     video.hide()
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/eZTf4ASiZ/model.json',modelLoaded);
 }
-function draw(){
 
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
